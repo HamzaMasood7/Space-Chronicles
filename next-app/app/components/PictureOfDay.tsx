5e236cba073f36e606221066ed9fd52afc628546
Rahul <87606332+Rahul-Palamarthi@users.noreply.github.com>
2023-10-04 14:19:20 +0530

    feat: add react-query for api calls (#17)

--- next-app/app/components/PictureOfDay.tsx
+++ next-app/app/components/PictureOfDay.tsx
@@ -1,67 +1,41 @@
-"use client";
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+'use client';
 
+import { getTodayPic } from '@/client/api';
+import { useQuery } from 'react-query';
 
-interface PicDay {
-  title: string;
-  explanation: string;
-  url: string;
-  hdUrl: string
-  date: string
+export default function PictureOfDay() {
+	const { data, isLoading, error } = useQuery('today-pic', getTodayPic);
+
+	// when the data is loading
+	if (isLoading) {
+		return (
+			<button type='button' className='btn btn-square'>
+				<span className='loading loading-spinner' />
+			</button>
+		);
+	}
+
+	// When there is no-data or error occured
+	if (!data || error) {
+		return <div>Error occured</div>;
+	}
+
+	return (
+		<div className='card w-96 bg-base-100 shadow-xl'>
+			<figure>
+				<img src={data.hdUrl} alt={data.title} />
+			</figure>
+			<div className='card-body text-center'>
+				<h2 className='card-title'>
+					{data.title}
+					{/* <div className="badge badge-secondary">{data.explanation}</div> */}
+				</h2>
+				{/* <p>{data.explanation}</p> */}
+				<div className='card-actions justify-end'>
+					{/* <div className="badge badge-outline">{data.explanation}</div> */}
+					<div className='badge badge-outline'>{data.date}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-
-const PictureOfDay = () => {
-
-  const [data, setData] = useState<PicDay | null>(null);
-  useEffect(() => {
-    const apiUrl = 'http://localhost:5000/earth/today';
-
-
-
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        const responseData: PicDay = response.data;
-        setData(responseData);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setData(null);
-      });
-  }, []);
-
-
-  return (
-    <div>
-      {data ? (
-        <>
-
-
-
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={data.hdUrl} alt={data.title} /></figure>
-            <div className="card-body text-center">
-              <h2 className="card-title">
-                {data.title}
-                {/* <div className="badge badge-secondary">{data.explanation}</div> */}
-              </h2>
-              {/* <p>{data.explanation}</p> */}
-              <div className="card-actions justify-end">
-                {/* <div className="badge badge-outline">{data.explanation}</div> */}
-                <div className="badge badge-outline">{data.date}</div>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : (
-        <button className="btn btn-square">
-          <span className="loading loading-spinner"></span>
-        </button>
-      )}
-    </div>
-  )
-}
-
-export default PictureOfDay
--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import ReactQueryProvider from '@/client/react-query-provider';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Space Chronicles',
-  description: 'Space Stuff',
-}
+	title: 'Space Chronicles',
+	description: 'Space Stuff',
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<ReactQueryProvider>{children}</ReactQueryProvider>
+			</body>
+		</html>
+	);
 }
